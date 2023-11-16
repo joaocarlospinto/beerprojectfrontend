252b063187ce5c81273f5cfd4660c6b702f2780f
joaocpinto <joao.pinto@ymail.com>
2023-11-16 17:04:24 +0000

    First fase

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,7 +15,7 @@ describe('AppComponent', () => {
   it(`should have as title 'beerprojectfrontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('beerprojectfrontend');
+    expect(app.pageTitle).toEqual('beerprojectfrontend');
   });
 
   it('should render title', () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule } from '@angular/router';
+import { BeerModule } from './beers/beer.module';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { RatingModule } from 'ngx-bootstrap/rating';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+            { path: 'about', component: AboutComponent},
+      { path: '', redirectTo: 'about', pathMatch: 'full'},
+      { path: '**', redirectTo: 'about', pathMatch: 'full'}
+    ]),
+    BeerModule,
+    ButtonsModule.forRoot(),
+    RatingModule.forRoot()  
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+}
